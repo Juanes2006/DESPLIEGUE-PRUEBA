9c30dc76f0f6a4fc8e87bad98f3e6f2e956aed14
Juanes2006 <juanestebanmarulanda117@gmail.com>
2025-06-09 21:29:40 -0500

    TERCER INTENTO

--- pr_evaluaciones/settings.py
+++ pr_evaluaciones/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-&68#yvvez*n^sfv3*0@^dhkyo=)^pr&9jy6ivfn#3a#4tik6ro
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','web-production-72711.up.railway.app']
 
 
 # Application definition
